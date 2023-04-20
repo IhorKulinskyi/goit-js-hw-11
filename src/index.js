2985af58e2166d47036dcedb94769ed3afca7842
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-04-20 16:48:09 +0300

    pagination

--- src/index.js
+++ src/index.js
@@ -1,35 +1,83 @@
-import fetchImages from './js/fetchImages';
+import ImageApiService from './js/ImageApiService';
 import { Notify } from 'notiflix';
-// import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
 };
 
 const FAILURE_MESSAGE =
   'Sorry, there are no images matching your search query. Please try again.';
 
+const imageApiService = new ImageApiService();
+
 refs.searchForm.addEventListener('submit', onSearchBtnClick);
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
+
+new SimpleLightbox('.gallery a');
 
 function onSearchBtnClick(e) {
   e.preventDefault();
-  clearGallery();
-  const searchQuery = e.target.elements.searchQuery.value.trim();
-  if (searchQuery === '') {
+  imageApiService.query = e.target.elements.searchQuery.value.trim();
+  imageApiService.resetPageCounter();
+  if (imageApiService.query === '') {
     Notify.info('Search input is empty');
     return;
   } else {
-    fetchImages(searchQuery).then(function (r) {
-      const images = r.data.hits;
-      if (images.length === 0) {
-        Notify.failure(FAILURE_MESSAGE);
-      } else {
-        const markup = images.map(img => createImageMarkup(img)).join('');
-        updateGallery(markup);
-      }
-    });
+    clearGallery();
+    searchImages();
+  }
+}
+
+function onLoadMoreBtnClick() {
+  imageApiService.fetchImages().then(r => onLoadMore(r));
+}
+
+function searchImages() {
+  imageApiService.fetchImages().then(r => onSearch(r));
+}
+
+function onSearch(r) {
+  refs.loadMoreBtn.classList.add('visually-hidden');
+  Notify.info(`Hooray! We found ${r.data.totalHits} images.`);
+  imageApiService.resetHits();
+  const images = r.data.hits;
+  console.log(r.data.totalHits === images.length);
+  imageApiService.addHits(images);
+  if (images.length === 0) {
+    Notify.failure(FAILURE_MESSAGE);
+  } else {
+    const markup = images.map(img => createImageMarkup(img)).join('');
+    updateGallery(markup);
+    if (images.length !== r.data.totalHits) {
+      refs.loadMoreBtn.classList.remove('visually-hidden');
+    }
+  }
+}
+
+function onLoadMore(r) {
+  const images = r.data.hits;
+  const totalHits = imageApiService.totalHits;
+  imageApiService.addHits(images);
+  const filteredHits = imageApiService.filterHits(totalHits);
+  const markup = images.map(img => createImageMarkup(img)).join('');
+  updateGallery(markup);
+
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+
+  if (filteredHits.length === r.data.totalHits) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
+    refs.loadMoreBtn.classList.add('visually-hidden');
   }
 }
 
